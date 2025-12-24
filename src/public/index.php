e81c3d7de999eda5b43cbdff2b0b5f69433e6226
sergi130706 <sergi13072006@gmail.com>
2025-12-24 16:31:36 +0100

    Entorno Laravel Base preparado para el desarrollo

--- src/public/index.php
+++ src/public/index.php
@@ -1,12 +1,20 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello World!</title>
-</head>
-<body>
-    <h1>¡Hola, mundo V2!</h1>
-    <?= phpinfo(); ?>
-</body>
-</html>
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+/** @var Application $app */
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
